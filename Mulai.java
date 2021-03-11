cff28122865dc117a45bc77f9887bd825e70390c
Andi Irham <Andi.irhamm@gmail.com>
2021-03-11 14:05:42 +0700

    init Mulai.java make the code for main and make function tampilkan

---
+++ Mulai.java
@@ -0,0 +1,12 @@
+public class Mulai {
+    public static void main(String[] args) {
+        System.out.println("Belajar Github");
+        System.out.println();
+
+        tampilkan("Hi, Andi");
+    }
+
+    public static void tampilkan(String nama){
+        System.out.println(nama);
+    }
+}
