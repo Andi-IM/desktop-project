8306a02e5c9358d64c032357442f897082cefda6
Hakkan Syukri <hakkansyukri13@gmail.com>
2021-03-11 22:04:36 +0700

    latihan commit

--- Mulai.java
+++ Mulai.java
@@ -1,2 +1,12 @@
-package PACKAGE_NAME;public class Mulai {
+public class Mulai {
+    public static void main(String[] args) {
+        System.out.println("Belajar Github");
+        System.out.println();
+        tampilkan("Hakkan");
+    }
+
+    public static void tampilkan(String nama){
+        System.out.println(nama);
+    }
 }
+
