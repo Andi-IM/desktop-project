90f417db0807d9c1602ec795963fb2906e09f057
Hakkan Syukri <hakkansyukri13@gmail.com>
2021-03-13 10:29:05 +0700

    Merging request to Main (#16)
    * Init Mulai.java
    Make a code for Main and make a function that displaying tampilkan(nama)
    
    * latihan commit
    
    * Revert "latihan commit"
    
    This reverts commit a36409a4ffcf733b912aa7e091915c74c0f285a2.
    
    * Revert "Revert "latihan commit""
    
    This reverts commit 7243cd385df0583dd2f5481d7d09e568fec8181f.
    
    * Init Minum.Java
    Added a code that fill wih function tampilkan
    
    * Init Hakkan.java
    Adding class Hakkan
    
    * init Hakkan.java
    latihan 2

---
+++ Hakkan.java
@@ -0,0 +1,7 @@
+public class Hakkan {
+    private  static String nama = "Hakkan";
+
+    public static  String getNama(){
+        return nama;
+    }
+}
---
+++ Mulai.java
@@ -0,0 +1,12 @@
+public class Mulai {
+    public static void main(String[] args) {
+        System.out.println("Belajar Github");
+        System.out.println();
+        tampilkan("Hakkan");
+    }
+
+    public static void tampilkan(String nama){
+        System.out.println(nama);
+    }
+}
+
