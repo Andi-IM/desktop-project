e40fe743c2722b93d10c0fe8d99b2daa9322e83c
Hakkan Syukri <hakkansyukri13@gmail.com>
2021-03-13 09:48:17 +0700

    init Hakkan.java latihan 2

--- Hakkan.java
+++ Hakkan.java
@@ -1,2 +1,7 @@
-package PACKAGE_NAME;public class Hakkan {
+public class Hakkan {
+    private  static String nama = "Hakkan";
+
+    public static  String getNama(){
+        return nama;
+    }
 }
